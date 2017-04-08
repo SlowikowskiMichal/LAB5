073cca1c1fdbe1c99c78208a8ffd0d07aebe5476
SlowikowskiMichal <slovik421@gmail.com>
2017-04-08 18:01:10 +0200

    Add files via upload

--- src/sample/AsyncTask.java
+++ src/sample/AsyncTask.java
@@ -4,35 +4,27 @@ import javafx.concurrent.Task;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 import java.util.Random;
 
-/**
- * Created by slovi on 03.04.2017.
- */
 public class AsyncTask extends Task {
-
-    private Canvas canvas;
+    private double hit;
+    private WritableImage writableImage;
     private GraphicsContext gc;
-    private PixelWriter pixelWriter;
-    private int count;
+    private long count;
     private ProgressBar progressBar;
 
-    public AsyncTask(GraphicsContext gcc, Canvas canvass, int c, ProgressBar pB) {
+    public AsyncTask(GraphicsContext gcc, long c, ProgressBar pB) {
 
+        hit = 0;
         progressBar = pB;
         count = c;
-        canvas = canvass;
         gc = gcc;
-        gc.setFill(Color.BLUEVIOLET);
-        gc.fillRect(gc.getCanvas().getLayoutX(),
-                gc.getCanvas().getLayoutY(),
-                gc.getCanvas().getWidth(),
-                gc.getCanvas().getHeight());
-
-        pixelWriter = gc.getPixelWriter();
+        writableImage = new WritableImage((int)gc.getCanvas().getWidth(),(int)gc.getCanvas().getWidth());
     }
 
     @Override
@@ -42,29 +34,26 @@ public class AsyncTask extends Task {
         Random random = new Random();
         double x;
         double y;
-        for(int i = 0; i < count; i++) {
+        for(long i = 0; i < count; i++) {
             x = -8 + 16*random.nextDouble();
             y = -8 + 16*random.nextDouble();
-            if(equation.calc(x,y))
-             pixelWriter.setColor((int)((x + 8) * canvas.getWidth()/16), (int) (-(y + 8) *canvas.getHeight()/16 + canvas.getHeight()), Color.YELLOW);
+            if(equation.calc(x,y)) {
+                writableImage.getPixelWriter().setColor((int)((x + 8) * gc.getCanvas().getWidth()/16), (int) (-(y + 8) * gc.getCanvas().getHeight()/16 + gc.getCanvas().getHeight()), Color.YELLOW);
+                hit++;
+            }
+            if(i%10000 == 0)
+            {
+                gc.drawImage(writableImage, 0, 0,gc.getCanvas().getWidth(),gc.getCanvas().getWidth());
+            }
+
             updateProgress(i,count);
         }
         updateProgress(1,1);
 
+        gc.drawImage(writableImage, 0, 0,gc.getCanvas().getWidth(),gc.getCanvas().getWidth());
+        System.out.println(256 * hit/ count);
 
-      //  for(double i = -8; i <= 8;i+=0.01)
-     //       for(double j = -8; j <= 8; j+=0.01)
-    //            if(equation.calc(i,j)) {
-   //                 pixelWriter.setColor((int)((i + 8) * canvas.getWidth()/16), (int) (-(j + 8) *canvas.getHeight()/16 + canvas.getHeight()), Color.YELLOW);
-  //              }
-
-     //   while (true)
-    //    {
-   //         System.out.println(".");
-  //          if (isCancelled()) break;
- //       }
-
-        return "Sukces";
+        return String.valueOf(256 * hit/ count);
     }
 
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,23 @@
 package sample;
 
-import javafx.application.Platform;
+
 import javafx.concurrent.WorkerStateEvent;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 
-import java.util.concurrent.TimeUnit;
 
 public class Controller {
 
     private AsyncTask task;
 
+    @FXML
+    private Label label;
     @FXML
     private ProgressBar progressBar;
     @FXML
@@ -26,59 +25,38 @@ public class Controller {
     @FXML
     private Canvas canvas;
     @FXML
+    private GraphicsContext gc;
+
+    @FXML
     private void handleRunBtnAction() {
 
-        int count = Integer.parseInt(textField.getText());
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        long count = Integer.parseInt(textField.getText());
+        gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLUEVIOLET);
+        gc.fillRect(gc.getCanvas().getLayoutX(),
+                gc.getCanvas().getLayoutY(),
+                gc.getCanvas().getWidth(),
+                gc.getCanvas().getHeight());
 
-        task = new AsyncTask(gc,canvas,count,progressBar);
+        task = new AsyncTask(gc,count,progressBar);
 
         progressBar.progressProperty().bind(task.progressProperty());
 
 
         //==============================================================================================================
 
-        task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent event) {
-                System.out.println((String) task.getValue());
-            }
-        });
+        task.setOnSucceeded(event -> label.setText((String)task.getValue()));
 
-        task.setOnCancelled(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent event) {
-                System.out.println("Przerwałem");
-            }
-        });
+        task.setOnCancelled(event -> System.out.println("Przerwałem"));
 
         //==============================================================================================================
 
         new Thread(task).start();
 
-     //   drawShapes(gc);
     }
     @FXML
     private void handleStopBtnAction(){
         if(!task.isCancelled())
             task.cancel();
-        //   drawShapes(gc);
     }
-
- /*   private void drawShapes(GraphicsContext gc) {
-        gc.setFill(Color.BLUEVIOLET);
-        gc.fillRect(gc.getCanvas().getLayoutX(),
-                        gc.getCanvas().getLayoutY(),
-                        gc.getCanvas().getWidth(),
-                        gc.getCanvas().getHeight());
-
-        PixelWriter pixelWriter = canvas.getGraphicsContext2D().getPixelWriter();
-        for(int i = 0; i < canvas.getWidth();i+=10)
-            for(int j = 1; j < canvas.getHeight(); j++)
-                pixelWriter.setColor(i, j, Color.YELLOW);
-        for(int i = 0; i < canvas.getWidth();i++)
-            for(int j = 1; j < canvas.getHeight(); j+=10)
-                pixelWriter.setColor(i, j, Color.YELLOW);
-    }*/
-
 }
